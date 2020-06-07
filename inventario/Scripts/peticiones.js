4d445fbbef621b4d16c43e1e6736167ac3f01d81
Ger0505 <gerzonex360@gmail.com>
2020-06-06 23:45:35 -0500

    Peticiones

---
+++ inventario/Scripts/peticiones.js
@@ -0,0 +1,17 @@
+﻿$("#IdPro").change(function () {
+    $.ajax({
+        url: "/inventario/Producto",
+        type: 'GET',
+        dataType: 'json',
+        contentType: 'application/json; charset=utf-8',
+        async: true,
+        success: function (result) {
+            console.log(result);
+            console.log(result.length);
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            alert("error al consultar datos del controlador");
+        }
+    });
+});
+$("#Total").prop('disabled', true);
